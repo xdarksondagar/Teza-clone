22278c618c9821abc9735a046c0ef0cdcb570f0d
Manthan Sondagar <manthan.sondagar@gmail.com>
2022-09-30 17:59:00 +0530

    complete till counter

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,10 @@
 // import Swiper bundle with all modules installed
+/* SMOOTH SCROLL
 import smoothScroll from "./scroll";
 
 const scroll__container = document.querySelector(".scroll__container");
 const scrolling = new smoothScroll(scroll__container);
-
+*/
 import Swiper from "swiper/bundle";
 
 // import styles bundle
@@ -34,4 +35,26 @@ btnContainers.forEach((btnContainer) => {
     gsap.to(hover, { top: "-400%" });
   });
   btn.addEventListener("mouseleave", () => gsap.set(hover, { top: "100%" }));
+});
+
+// counter update
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach((counter) => {
+  counter.innerText = "0+";
+
+  const updateCounter = () => {
+    const c = +counter.innerText;
+    const target = +counter.getAttribute("data-target");
+
+    const increment = target / 500;
+
+    if (c < target) {
+      counter.innerText = `${Math.ceil(c + increment)}+`;
+      setTimeout(updateCounter, 1);
+    } else {
+      counter.innerText = target + "+";
+    }
+  };
+  updateCounter();
 });
